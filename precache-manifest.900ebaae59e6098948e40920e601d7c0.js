bfd73bcf636488279882c428b4dc4f755f5c3637
akash-yede <ayakashyede@gmail.com>
2020-04-05 19:04:59 -0400

    Updates

---
+++ precache-manifest.900ebaae59e6098948e40920e601d7c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb48d6f88b09c5a78b528cc0b88084b0",
+    "url": "/movie-dictionary/index.html"
+  },
+  {
+    "revision": "a68f7709f1511467cd00",
+    "url": "/movie-dictionary/static/css/main.74e56703.chunk.css"
+  },
+  {
+    "revision": "0f4d602c11f3fbf1a411",
+    "url": "/movie-dictionary/static/js/2.529d4730.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/movie-dictionary/static/js/2.529d4730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68f7709f1511467cd00",
+    "url": "/movie-dictionary/static/js/main.603e7191.chunk.js"
+  },
+  {
+    "revision": "4512860bab1f1a7f31f4",
+    "url": "/movie-dictionary/static/js/runtime-main.35a2964e.js"
+  },
+  {
+    "revision": "dbeecda6286b0ee37103bd0f7eca9df2",
+    "url": "/movie-dictionary/static/media/spinner.dbeecda6.gif"
+  }
+]);
